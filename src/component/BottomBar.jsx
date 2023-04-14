ee55195e49eaa5d71fe6cda9c7f5c80241072c28
Vincent Vuram <vincentvuram204@gmail.com>
2023-04-14 16:58:03 +0530

    bottombar icons size increased

--- src/component/BottomBar.jsx
+++ src/component/BottomBar.jsx
@@ -34,21 +34,21 @@ const BottomBar = () => {
     <div className='sm:hidden max-w-screen w-full fixed bottom-0 h-fit bg-white flex justify-evenly items-center content-center p-1 gap-1 z-20 border-t border-slate-500'> 
          <Link to={'/home'} className={`w-fit   p-1 ${location === '/home' ? 'bg-slate-200' : 'bg-white'} hover:bg-slate-300 rounded-md cursor-pointer stroke-1 flex   items-center gap-2 text-md font-semibold `}> 
             {location === '/home' ? 
-              <RiHomeFill size={25}/> :
-              <RiHomeLine size={25}/>
+              <RiHomeFill size={30}/> :
+              <RiHomeLine size={30}/>
             } 
           </Link>
           <Link to={'/discover'} className={`w-fit   p-1 ${location === '/discover' ? 'bg-slate-200' : 'bg-white'} hover:bg-slate-300 rounded-md cursor-pointer stroke-1 flex items-center gap-2 text-md font-semibold  `}>
             {location === '/discover' ? 
-              <AiFillCompass size={25}/> :
-              <AiOutlineCompass size={25}/>
+              <AiFillCompass size={30}/> :
+              <AiOutlineCompass size={30}/>
             } 
           </Link> 
           <button className='w-fit   p-1 hover:bg-slate-300 rounded-md cursor-pointer flex items-center gap-2 font-semibold' onClick={()=>setCreatePostModal(!createPostModal)}>
-            <GrAddCircle size={25} strokeWidth={3}/>  
+            <GrAddCircle size={30} strokeWidth={3}/>  
           </button>   
           <Link to={`/profile/${currentUserState.username}`} className={`w-fit   p-1 ${location === '/discover' ? 'bg-slate-200' : 'bg-white'} hover:bg-slate-300 rounded-md cursor-pointer stroke-1 flex items-center gap-2 text-md font-semibold  `}>
-            <div className='w-[25px] h-[25px] bg-slate-600 rounded-full'>
+            <div className='w-[30px] h-[30px] bg-slate-600 rounded-full'>
             { currentUserState.avatar.url==='' ?  
                         currentUserState.firstname[0]
                         :
